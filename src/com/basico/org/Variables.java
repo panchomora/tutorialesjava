ac96abb7cbcf18ee5e3006e777ae3a840d9fd368
Francisco Mora <contacto@panchomora.cl>
2013-11-30 01:18:46 -0300

    Se elimina Main.java y Main.class

---
+++ src/com/basico/org/Variables.java
@@ -0,0 +1,12 @@
+package com.basico.org;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		String saludo = "Hola";
+		
+		System.out.println("El valor de num1 es: " + num1);
+		System.out.println("el valor de saludo es: "+ saludo);
+	}
+}
