24d1a20cf7c4ccd6be5ce5c3cb1359a6aea88ae2
Francisco Mora <contacto@panchomora.cl>
2013-11-30 01:33:55 -0300

    se completa trabajo con variables

--- src/com/basico/org/Variables.java
+++ src/com/basico/org/Variables.java
@@ -8,5 +8,13 @@ public class Variables {
 		
 		System.out.println("El valor de num1 es: " + num1);
 		System.out.println("el valor de saludo es: "+ saludo);
+		
+		int a = 15;
+		int b = 10;
+		
+		System.out.printf("La suma entre %d y %d es: %d", a,b,(a+b));
+		System.out.printf("\nLa resta entre %d y %d es: %d", a,b,(a-b));
+		System.out.printf("\nLa multiplicacion entre %d y %d es: %d", a,b,(a*b));
+		System.out.printf("\nLa division entera entre %d y %d es: %d", a,b,(a/b));
 	}
 }
